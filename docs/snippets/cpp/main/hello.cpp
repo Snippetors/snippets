ae1eff86893596a3c005ecfce106366d598056f5
Lucien <lucienzhangzl@outlook.com>
2021-09-13 17:43:36 +0900

    feat(ci): add cpp lint (close #22)

--- docs/snippets/cpp/main/hello.cpp
+++ docs/snippets/cpp/main/hello.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     cout << "Hello Snippets!" << endl;
     return 0;
 }
