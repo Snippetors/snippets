cd243cefef66d2603de2f4293c27f9b18a29b224
Lucien <lucienzhangzl@outlook.com>
2021-09-16 11:57:04 +0900

    local plugin bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,29 @@
-const path = require("path");
+const { path } = require("@vuepress/utils");
 
 module.exports = {
   lang: "zh-Hans-CN",
   title: "Snippets",
   description: "即抄即用 Copy & Run",
-  plugins: [[require("../../packages/@snippetors/vuepress-plugin-tabs"), {}]],
+  plugins: [
+    [
+      (() => {
+        let res = path
+          .resolve(__dirname, "../../packages/@snippetors/vuepress-plugin-tabs")
+          .split(path.sep)
+          .join("/");
+        console.log(
+          "the path is",
+          res,
+          path.resolve(
+            __dirname,
+            "../../packages/@snippetors/vuepress-plugin-tabs"
+          )
+        );
+        return res;
+      })(),
+      {},
+    ],
+  ],
   markdown: {
     importCode: {
       handleImportPath: (str) =>
