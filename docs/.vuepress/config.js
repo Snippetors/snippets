c2cc7608a781b411a0b1d51fb12bca106d25af52
Lucien <lucienzhangzl@outlook.com>
2021-09-17 20:44:20 +0900

    feat(core): add toc (close #27)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,14 +20,76 @@ module.exports = {
     repo: "Snippetors/snippets",
     docsDir: "docs",
     navbar: [
-      { text: "简介", link: "/" },
-      { text: "杂项", link: "/misc/hello" },
+      { text: "简介", link: "/intro/" },
+      { text: "基础算法", link: "/basic/" },
+      { text: "数据结构", link: "/data_structure/" },
+      { text: "搜索", link: "/search/" },
+      { text: "动态规划", link: "/dp/" },
+      { text: "字符串", link: "/string/" },
+      { text: "数学", link: "/math/" },
+      { text: "图论", link: "/graph/" },
+      { text: "计算几何", link: "/geometry/" },
+      { text: "杂项", link: "/misc/" },
     ],
     sidebar: {
+      "/intro/": [
+        {
+          title: "简介",
+          children: ["/intro/"],
+        },
+      ],
+      "/basic/": [
+        {
+          title: "基础算法",
+          children: ["/basic/"],
+        },
+      ],
+      "/data_structure/": [
+        {
+          title: "数据结构",
+          children: ["/data_structure/"],
+        },
+      ],
+      "/search/": [
+        {
+          title: "搜索",
+          children: ["/search/"],
+        },
+      ],
+      "/dp/": [
+        {
+          title: "动态规划",
+          children: ["/dp/"],
+        },
+      ],
+      "/string/": [
+        {
+          title: "字符串",
+          children: ["/string/"],
+        },
+      ],
+      "/math/": [
+        {
+          title: "数学",
+          children: ["/math/"],
+        },
+      ],
+      "/graph/": [
+        {
+          title: "图论",
+          children: ["/graph/"],
+        },
+      ],
+      "/geometry/": [
+        {
+          title: "计算几何",
+          children: ["/geometry/"],
+        },
+      ],
       "/misc/": [
         {
           title: "杂项",
-          children: ["/misc/hello"],
+          children: ["/misc/", "/misc/hello"],
         },
       ],
     },
