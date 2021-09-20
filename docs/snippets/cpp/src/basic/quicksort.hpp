b542abfeaba0f9d324d5f676d596f6bd38e2c907
LIUDINGYU <liudy.zju@gmail.com>
2021-09-20 16:05:31 +0800

    add quicksort cpp version

--- docs/snippets/cpp/src/basic/quicksort.hpp
+++ docs/snippets/cpp/src/basic/quicksort.hpp
@@ -4,14 +4,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <utility>
 
 using namespace std;
-
 int partition(vector<int>& nums, int left, int right) {
     if (left >= right) { return left; }
     int pivot = nums[left], pos = left;
-    // pivot: move all numbers smaller than pivot to left, and larger than pivot to right 
-    // pos: index the end of left part (containing numbers smaller than pivot)
     for (int i = left + 1; i <= right; ++i) {
         if (nums[i] < pivot) {
             swap(nums[++pos], nums[i]);
@@ -21,7 +19,8 @@ int partition(vector<int>& nums, int left, int right) {
     return pos;
 }
 
-void quicksort(vector<int>& nums, int left, int right) { // ±ÕÇø¼ä [left, right] 
+void quicksort(vector<int>& nums, int left, int right) {  
+    //closed interval [left, right] 
     if (left < right) {
         int pos = partition(nums, left, right);
         quicksort(nums, left, pos - 1);
