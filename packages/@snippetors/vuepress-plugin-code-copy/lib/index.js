75c1e92fde8f49a7e87d3c346024569999ae4b71
Lucien <lucienzhangzl@outlook.com>
2022-01-19 00:18:25 +0900

    feat(plugin:code-copy): avoid polluting global namespace

--- packages/@snippetors/vuepress-plugin-code-copy/lib/index.js
+++ packages/@snippetors/vuepress-plugin-code-copy/lib/index.js
@@ -2,14 +2,16 @@ const { path } = require("@vuepress/utils");
 
 module.exports = (options = {}, app) => ({
   define: {
-    selector: options.selector || 'div[class*="language-"]',
-    align: options.align || "bottom",
-    color: options.color || "#27b1ff",
-    backgroundColor: options.backgroundColor || "#0075b8",
-    backgroundTransition: options.backgroundTransition !== false,
-    successText: options.successText || "Copied!",
-    successTextColor: options.successTextColor || options.color || "#27b1ff",
-    staticIcon: options.staticIcon === true,
+    snippetorsCodeCopyOptions: {
+      selector: options.selector || 'div[class*="language-"]',
+      align: options.align || "bottom",
+      color: options.color || "#27b1ff",
+      backgroundColor: options.backgroundColor || "#0075b8",
+      backgroundTransition: options.backgroundTransition !== false,
+      successText: options.successText || "Copied!",
+      successTextColor: options.successTextColor || options.color || "#27b1ff",
+      staticIcon: options.staticIcon === true,
+    },
   },
   name: "@snippetors/vuepress-plugin-code-copy",
   multiple: false,
