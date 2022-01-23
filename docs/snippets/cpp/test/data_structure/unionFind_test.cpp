b791fa8e2565c352330e7c6db3837507ac882c68
LIUDINGYU <liudy.zju@gmail.com>
2022-01-23 18:12:28 +0800

    fix test case

--- docs/snippets/cpp/test/data_structure/unionFind_test.cpp
+++ docs/snippets/cpp/test/data_structure/unionFind_test.cpp
@@ -7,10 +7,10 @@ TEST(UnionFindTest, BasicAssertions) {
     uf->join(0, 1);
     uf->join(1, 2);
     uf->join(3, 4);
-    uf->join(3, 10);
+    uf->join(3, 9);
 
     EXPECT_TRUE(uf->join(0, 2));
     EXPECT_FALSE(uf->join(0, 5));
-    EXPECT_TRUE(uf->join(3, 10));
+    EXPECT_TRUE(uf->join(3, 9));
     EXPECT_FALSE(uf->join(4, 5));
 }
